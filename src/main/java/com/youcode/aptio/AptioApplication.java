e5fbdc6490276c61c3a6b87cb12d9d1f380cf2dd
LahcenBenAmar <lahcen.ben3@gmail.com>
2024-12-27 10:09:44 +0100

    set up the project

---
+++ src/main/java/com/youcode/aptio/AptioApplication.java
@@ -0,0 +1,13 @@
+package com.youcode.aptio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AptioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youcode/aptio/AptioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youcode.aptio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
