6a2d20e51211fe98f9d828acdf10104eab7df87c
LahcenBenAmar <lahcen.ben3@gmail.com>
2024-12-31 15:43:57 +0100

    feat: add timezone validation utility and annotation for input validation

--- src/main/java/com/youcode/aptio/validation/IsValidTimezoneValidator.java
+++ src/main/java/com/youcode/aptio/validation/IsValidTimezoneValidator.java
@@ -1,4 +1,17 @@
 package com.youcode.aptio.validation;
 
-public class IsValidTimezoneValidator {
+import com.youcode.aptio.util.TimezoneUtils;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class IsValidTimezoneValidator implements ConstraintValidator<IsValidTimezone, String> {
+
+    @Override
+    public void initialize(IsValidTimezone constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(String timezone, ConstraintValidatorContext context) {
+        return TimezoneUtils.isValidTimezone(timezone);
+    }
 }
