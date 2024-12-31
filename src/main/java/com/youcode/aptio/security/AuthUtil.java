a0f4c57f10c505db5e8e14365541885f54ddfcc7
LahcenBenAmar <lahcen.ben3@gmail.com>
2024-12-31 15:07:47 +0100

    feat: implement authentication utilities and user details service for role management

--- src/main/java/com/youcode/aptio/security/AuthUtil.java
+++ src/main/java/com/youcode/aptio/security/AuthUtil.java
@@ -1,4 +1,42 @@
 package com.youcode.aptio.security;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+
 public class AuthUtil {
+
+    public static String getAuthUsername() {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
+
+    public static boolean isBusinessOwner() {
+        return SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream()
+                .anyMatch(a -> a.getAuthority().equals("ROLE_BUSINESS_OWNER"));
+    }
+
+    public static boolean isEmployee() {
+        return SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream()
+                .anyMatch(a -> a.getAuthority().equals("ROLE_EMPLOYEE"));
+    }
+
+    public static boolean isCustomer() {
+        return SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream()
+                .anyMatch(a -> a.getAuthority().equals("ROLE_CUSTOMER"));
+    }
+
+    public static boolean isAdmin() {
+        return SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream()
+                .anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
+    }
+
+    public static boolean isAnonymous() {
+        return SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream()
+                .anyMatch(a -> a.getAuthority().equals("ROLE_ANONYMOUS"));
+    }
+
+    public static boolean isAuthenticated() {
+        return SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream()
+                .anyMatch(a -> a.getAuthority().equals("ROLE_USER"));
+    }
+
+
 }
