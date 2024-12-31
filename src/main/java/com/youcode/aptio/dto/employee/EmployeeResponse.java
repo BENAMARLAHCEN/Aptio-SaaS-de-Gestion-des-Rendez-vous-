f597bc470f2a4a705bdef6b7970cb7da6702e62b
LahcenBenAmar <lahcen.ben3@gmail.com>
2024-12-31 15:44:31 +0100

    feat: enhance DTOs for Business, Employee, and Service with validation and mapping methods

--- src/main/java/com/youcode/aptio/dto/employee/EmployeeResponse.java
+++ src/main/java/com/youcode/aptio/dto/employee/EmployeeResponse.java
@@ -1,4 +1,21 @@
 package com.youcode.aptio.dto.employee;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeResponse {
+    private Long id;
+    private Long userId;
+    private Long businessId;
+    private String position;
+    private boolean isActive;
+    private LocalDateTime createdAt;
 }
--- src/main/java/com/youcode/aptio/util/mapper/BusinessMapper.java
+++ src/main/java/com/youcode/aptio/util/mapper/BusinessMapper.java
@@ -1,4 +1,17 @@
 package com.youcode.aptio.util.mapper;
 
+import com.youcode.aptio.dto.business.BusinessRequest;
+import com.youcode.aptio.dto.business.BusinessResponse;
+import com.youcode.aptio.model.Business;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
 public interface BusinessMapper {
+    Business toBusiness(BusinessRequest businessRequest);
+    BusinessResponse toBusinessResponse(Business business);
+
+    // add a method to update business
+    // Business updateBusiness(Business business, BusinessRequest businessRequest);
+    void updateBusinessFromRequest(BusinessRequest businessRequest, @MappingTarget Business business);
 }
