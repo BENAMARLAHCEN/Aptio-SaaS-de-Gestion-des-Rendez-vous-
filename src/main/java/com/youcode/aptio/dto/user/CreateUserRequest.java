13714bb7b1c663a4493aba5311e0af3fe46464d7
LahcenBenAmar <lahcen.ben3@gmail.com>
2025-01-12 13:06:16 +0100

    feat: add user-related request DTOs with validation for password change and profile updates

--- src/main/java/com/youcode/aptio/dto/user/CreateUserRequest.java
+++ src/main/java/com/youcode/aptio/dto/user/CreateUserRequest.java
@@ -1,4 +1,35 @@
 package com.youcode.aptio.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateUserRequest {
+    @NotEmpty(message = "First name is required")
+    private String firstName;
+
+    @NotEmpty(message = "Last name is required")
+    private String lastName;
+
+    @NotEmpty(message = "Username is required")
+    private String username;
+
+    @Email(message = "Invalid email format")
+    @NotEmpty(message = "Email is required")
+    private String email;
+
+    @NotEmpty(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    private String password;
+
+    private String phone;
+    private String role;
 }
--- src/main/java/com/youcode/aptio/dto/user/UpdateProfileRequest.java
+++ src/main/java/com/youcode/aptio/dto/user/UpdateProfileRequest.java
@@ -1,4 +1,16 @@
 package com.youcode.aptio.dto.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateProfileRequest {
+    private String firstName;
+    private String lastName;
+    private String phone;
 }
