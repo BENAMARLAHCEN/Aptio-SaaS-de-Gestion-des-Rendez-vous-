fe0c762026df809f780ede0f07e8f90f2670088b
LahcenBenAmar <lahcen.ben3@gmail.com>
2025-01-13 10:06:04 +0100

    feat: add custom exception classes for account verification and authentication errors

---
+++ src/main/java/com/youcode/aptio/exception/MaxLoginAttemptsExceededException.java
@@ -0,0 +1,7 @@
+package com.youcode.aptio.exception;
+
+public class MaxLoginAttemptsExceededException extends RuntimeException {
+  public MaxLoginAttemptsExceededException(String message) {
+    super(message);
+  }
+}
