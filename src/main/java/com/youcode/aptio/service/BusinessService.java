d9287ccf065c4b05afacab136633aded37011cdb
LahcenBenAmar <lahcen.ben3@gmail.com>
2025-01-19 15:01:17 +0100

    Refactor updateBusiness to use BusinessUpdateRequest
    Replaced BusinessRequest with BusinessUpdateRequest in updateBusiness flow for better request distinction and handling. Updated relevant layers including service, controller, and mapper to align with this change. Improved data mapping by adding specific field mappings in the BusinessMapper.

--- src/main/java/com/youcode/aptio/service/BusinessService.java
+++ src/main/java/com/youcode/aptio/service/BusinessService.java
@@ -2,12 +2,13 @@ package com.youcode.aptio.service;
 
 import com.youcode.aptio.dto.business.BusinessRequest;
 import com.youcode.aptio.dto.business.BusinessResponse;
+import com.youcode.aptio.dto.business.BusinessUpdateRequest;
 
 import java.util.List;
 
 public interface BusinessService {
     BusinessResponse createBusiness(BusinessRequest request);
-    BusinessResponse updateBusiness(Long id, BusinessRequest request);
+    BusinessResponse updateBusiness(Long id, BusinessUpdateRequest request);
     void deleteBusiness(Long id);
     BusinessResponse assignOwner(Long businessId, Long userId);
     BusinessResponse getBusinessById(Long id);
--- src/main/java/com/youcode/aptio/util/mapper/BusinessMapper.java
+++ src/main/java/com/youcode/aptio/util/mapper/BusinessMapper.java
@@ -2,16 +2,19 @@ package com.youcode.aptio.util.mapper;
 
 import com.youcode.aptio.dto.business.BusinessRequest;
 import com.youcode.aptio.dto.business.BusinessResponse;
+import com.youcode.aptio.dto.business.BusinessUpdateRequest;
 import com.youcode.aptio.model.Business;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface BusinessMapper {
     Business toBusiness(BusinessRequest businessRequest);
+
+    @Mapping(target = "ownerId", source = "owner.id")
+    @Mapping(target = "ownerName", source = "owner.username")
     BusinessResponse toBusinessResponse(Business business);
 
-    // add a method to update business
-    // Business updateBusiness(Business business, BusinessRequest businessRequest);
-    void updateBusinessFromRequest(BusinessRequest businessRequest, @MappingTarget Business business);
+    void updateBusinessFromRequest(BusinessUpdateRequest businessRequest, @MappingTarget Business business);
 }
